a3c5eac5c1d3a3ff1e2620eff9043a20d982d4fe
UNTEJANNAVARAM <annavaramuntej>
2025-08-31 10:34:15 +0530

    commit

--- src/app/components/asset-view/asset-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-asset-view',
-  templateUrl: './asset-view.component.html',
-  styleUrls: ['./asset-view.component.css'],
-  standalone: true,
-})
-export class AssetViewComponent { }
